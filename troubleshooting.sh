596feabd79bed1d62e991be18ee1994dfa837ebb
root <kbj9704@gmail.com>
2020-07-06 08:40:20 +0000

    move script

--- troubleshooting.sh
+++
@@ -1,8 +0,0 @@
-swapoff -a
-strace -eopenat kubectl version
-sudo systemctl restart kubelet.service
-sudo systemctl start docker
-sudo systemctl start kubelet
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
